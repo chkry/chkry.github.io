f0874b67f7880ecf48baf77f1f3554e09393f5c3
Chakri Reddy <chkryreddy@gmail.com>
2016-02-08 20:09:28 +0530

    [chkry] Added main desc color and fixed font

--- js/scripts.js
+++ js/scripts.js
@@ -5,43 +5,36 @@
        $("#descri").fadeIn(1000);
        $("#social").fadeIn(1000);
 
-//Srolling to division functions.........................................
+       //Srolling to division functions.........................................
        $("#logo").click(function() {
            $('html, body').animate({
                scrollTop: $("#third").offset().top
            }, 3000);
        });
 
-
-
-              $("#blogButton").click(function() {
+       $("#blogButton").click(function() {
            $('html, body').animate({
                scrollTop: $("#fourth").offset().top
            }, 3000);
        });
-
-
-
 
        $("#descri").click(function() {
            $('html, body').animate({
                scrollTop: $("#fourth").offset().top
            }, 6000);
        });
-       
-              $("#social").click(function() {
+
+       $("#social").click(function() {
            $('html, body').animate({
                scrollTop: $("#fourth").offset().top
            }, 3000);
        });
 
-//Srolling to division functions.........................................
+       //Srolling to division functions.........................................
        var n = $("#main div").length;
        var g = $("#second").length;
        var f = $("#third").length;
-       // console.log("n=" + n);
-       // console.log("second=" + g);
-       // console.log("tird" + f);
+
 
 
 
@@ -62,53 +55,6 @@
                }
            }
        });
-
-
-
-       /* Sixth division visibility scrolling */
-      // $(window).scroll(function() {
-      //      if ($('#main').visible(true) == true && $('#third').visible(true) == false) {
-      //           console.log("main now checking showing");
-      //          $("#sixth").css("display", "none");
-      //      } else {
-      //           console.log("6th now chae not showing");
-      //          if ($('#third').visible()==false && $('#fourth').visible(true) && $('#fifth').visible(true)) {
-      //              $("#sixth").show();
-      //              if($('#third').visible(true)){
-      //                $("#sixth").css("display", "none");
-      //              }
-      //          }
-      //      }
-      //  });
-
-// $(window).scroll(function() {
-//       if($('#fifth').visible(true)){
-//         if('#third').visible()==false){
-//           $("#fifth").css("z-index", "0");
-//           $("#sixth").css("z-index", "1");
-//         }else{
-//           $("#fifth").css("z-index", "-900");
-//           $("#sixth").css("z-index", "0");
-//         }
-//       }
-
-//   });
-
-
-/*       $(window).scroll(function() {
-           if ($('#fourth').visible(true) && $('#fifth').visible(true) && $("#third").visible(true) == false) {
-
-               $("#sixth").show().slideDown(4000);
-
-               console.log("normal is going on");
-
-           } else {
-             // $("#sixth").slideUp(4000);
-               $("#sixth").hide().css("display", "none");
-                console.log("else is going on");
-           }
-       });*/
-
 
 
    });
