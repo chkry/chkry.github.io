aca9bb63cb2c3dcf9f2838c8238ca174a54ed04f
Chakri Reddy <chkryreddy@gmail.com>
2016-08-16 18:53:41 +0530

    First Page One time Scroll working fine

--- js/scripts.js
+++ js/scripts.js
@@ -17,12 +17,13 @@ $("document").ready(function () {
 
 
 
-    //Srolling to division functions.........................................
+    //Srolling to division on click functions.........................................
     $("#logo").click(function () {
         $('html, body').animate({
             scrollTop: third.offset().top
         }, 1200);
     });
+
     $("#aboutButton").click(function () {
         $('html, body').animate({
             scrollTop: thirdone.offset().top
@@ -39,6 +40,25 @@ $("document").ready(function () {
             scrollTop: main.offset().top
         }, 2000);
     });
+    var counter = true;
+    $(window).scroll(function () {
+
+        while(counter){
+            if(main.visible(true)){
+            $('html, body').animate({
+            scrollTop: thirdone.offset().top
+        }, 2000);
+        }
+        counter = false;
+        }
+
+    });
+
+//     main.click(function () {
+//        $('html, body').animate({
+//            scrollTop: thirdone.offset().top
+//        }, 2000);
+//    });
 
 
     //       $("#social").click(function() {
